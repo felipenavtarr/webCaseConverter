282ddf4391a15ff0312f3fca44f4b17477a39525
felipenavtarr <felipenavtarr@gmail.com>
2022-06-30 17:30:50 +0200

    Step 3/3

---
+++ download-text.js
@@ -0,0 +1,17 @@
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+document.getElementById('save-text-file').addEventListener('click', () => {
+    let text = document.getElementsByTagName("textarea")[0].value;
+    download("text.txt", text)
+});
